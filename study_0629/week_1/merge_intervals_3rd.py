775ffab8a6dc2ab26761f98cb6529c456d9a3d93
baekho <klyhyeon@gmail.com>
2026-07-05 10:31:25 +0900

    feat: 최종 코드와 후기 작성

--- study_0629/week_1/merge_intervals_3rd.py
+++ study_0629/week_1/merge_intervals_3rd.py
@@ -28,11 +28,6 @@
 # intervals[i].length == 2
 # 0 <= starti <= endi <= 104
 
-# 겹치는 구간을 병합해서 구간들이 서로 겹치지 않도록 반환
-# 첫 번째 시도와 달리 기존 리스트에서 제거하지 않고 신규 리스트를 추가하거나 다른 방법 사용
-# 소요시간: 
-# 예외: [1,4],[0,2],[3,5]
-
 from typing import List
 
 
@@ -40,18 +35,19 @@ class Solution:
 
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         answer: List = list()
-        # 정렬하면 바로 뒷 숫자 배열과 answer의 맨 나중의 배열만 비교하면 된다
-        intervals.sort(key = lambda x : x[0])
+        # 정렬하면 바로 뒷 숫자 배열과 answer의 맨 나중의 배열만 비교하면 된다        
+        intervals.sort(key = lambda x : (x[0], x[1]))
         for index, item in enumerate(intervals):
-            if answer and answer[-1][1] >= item[1]:
-                continue
+            if answer and answer[-1][1] >= item[0]:
+                new_answer = [answer[-1][0], max(answer[-1][1], item[1])]
+                answer.pop()
+                answer.append(new_answer)
+            else:
+                answer.append(item)
+            # 마지막 숫자 처리
             if index == len(intervals) - 1:
-                if answer and answer[-1][1] >= item[1]:
-                    answer.append(intervals[index + 1])
-                    break
-                else:
-                    answer.append(item)
-                    break
+                break
+            # index + 1 처리
             if item[1] >= intervals[index + 1][0]:
                 if answer and answer[-1][1] >= intervals[index + 1][0]:
                     new_answer = [answer[-1][0], max(answer[-1][1], intervals[index + 1][1])]
@@ -59,12 +55,10 @@ class Solution:
                     answer.append(new_answer)
                 else:
                     answer.append([item[0], max(item[1], intervals[index + 1][1])])
-            else:
-                answer.append(item)
         return answer
 
 sol = Solution()
-print(sol.merge([[1,4],[0,2],[3,5]]))
+print(sol.merge([[3,5],[1,1],[0,2],[4,5],[0,2],[0,0],[5,7],[1,1],[3,3],[0,2],[2,3]]))
 
 
 # self, 클래스 내 메서드 관련 내용 공부
