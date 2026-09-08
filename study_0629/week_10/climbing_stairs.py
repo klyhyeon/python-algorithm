7d0fa847f4bf27b389559a13e5c9b538d297d781
yuhyeon <klyhyeon@gmail.com>
2026-09-08 17:42:55 +0900

    feat: LeetCode climbing stairs

---
+++ study_0629/week_10/climbing_stairs.py
@@ -0,0 +1,41 @@
+# You are climbing a staircase. It takes n steps to reach the top.
+
+# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+ 
+
+# Example 1:
+
+# Input: n = 2
+# Output: 2
+# Explanation: There are two ways to climb to the top.
+# 1. 1 step + 1 step
+# 2. 2 steps
+# Example 2:
+
+# Input: n = 3
+# Output: 3
+# Explanation: There are three ways to climb to the top.
+# 1. 1 step + 1 step + 1 step
+# 2. 1 step + 2 steps
+# 3. 2 steps + 1 step
+ 
+# Constraints:
+
+# 1 <= n <= 45
+
+# 힌트 보고 풀이. 이전까지 경우의 수 합이 현재 경우의 수가 된다.
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [0] * (n+1)
+        dp[1] = 1
+        if n == 1:
+            return dp[1]
+        dp[2] = 2
+
+        for i in range(3, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
+
+print(Solution().climbStairs(45))
